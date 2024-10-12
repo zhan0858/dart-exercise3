589e836aca1fa4f9271d09403bc278cfe8f06449
zhan0858 <zhan0858@algonquinlive.com>
2024-10-12 16:54:48 -0400

    add init project:

---
+++ bin/exercise3.dart
@@ -0,0 +1,5 @@
+import 'package:exercise3/exercise3.dart' as exercise3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exercise3.calculate()}!');
+}
---
+++ test/exercise3_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercise3/exercise3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
